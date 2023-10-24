0d4eb0caf50a75985731062e64d5412ccf2e559d
larsbarstad <lmlovold@stud.ntnu.no>
2023-10-24 13:39:38 +0200

    Made changes to the RemoteControl class to accomodate the additional commands

--- src/main/java/no/ntnu/RemoteControl.java
+++ src/main/java/no/ntnu/RemoteControl.java
@@ -15,6 +15,14 @@ public class RemoteControl {
   private Socket socket;
   private BufferedReader socketReader;
   private PrintWriter socketWriter;
+  public static final String CHANNEL_COUNT_COMMAND = "count";
+  public static final String TURN_ON_COMMAND = "1";
+  public static final String OK_RESPONSE = "ok";
+  public static final String TURN_OFF_COMMAND = "2";
+  public static final String CHANNEL_UP_COMMAND = "up";
+  public static final String CHANNEL_DOWN COMMAND = "down";
+  public static final String SET_CHANNEL_COMAND = "set";
+  public static final String GET_STATUS_COMMAND = "get";
 
   public static void main(String[] args) {
     RemoteControl remoteControl = new RemoteControl();
@@ -27,7 +35,26 @@ public class RemoteControl {
       socketWriter = new PrintWriter(socket.getOutputStream(), true);
       socketReader = new BufferedReader(
           new InputStreamReader(socket.getInputStream()));
+      // Test turning on the TV
+      sendCommandToServer(TURN_ON_COMMAND);
 
+      // Test getting channel count
+      sendCommandToServer(CHANNEL_COUNT_COMMAND);
+
+      // Test increasing the channel
+      sendCommandToServer(CHANNEL_UP_COMMAND);
+
+      // Test decreasing the channel
+      sendCommandToServer(CHANNEL_DOWN_COMMAND);
+
+      // Test setting a specific channel (e.g., channel 5)
+      sendCommandToServer("s5");
+
+      // Test getting TV status
+      sendCommandToServer(GET_STATUS_COMMAND);
+
+      // Test turning off the TV
+      sendCommandToServer(TURN_OFF_COMMAND);
       sendCommandToServer("c");
       sendCommandToServer("1");
       sendCommandToServer("c");
