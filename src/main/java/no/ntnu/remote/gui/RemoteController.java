3c8c5076d30c2c35514fdb6a4fe1a1492f23b7bf
Anders Lund <61545005+AnderzL7@users.noreply.github.com>
2023-11-02 15:59:30 +0100

    Implements controller methods to update model

--- src/main/java/no/ntnu/remote/gui/RemoteController.java
+++ src/main/java/no/ntnu/remote/gui/RemoteController.java
@@ -10,27 +10,18 @@ public class RemoteController {
     }
 
     public void turnOnTV() {
-        //model.turnOnTV();
+        model.turnOnTV();
     }
 
     public void turnOffTV() {
-        // Communicate with model to turn off the TV
-        // Update the view's responseArea with the server's response
+        model.turnOffTV();
     }
 
     public void channelUp() {
-        // Communicate with model to increase channel
-        // Update the view's responseArea with the server's response
+        model.channelUp();
     }
 
     public void channelDown() {
-        // Communicate with model to decrease channel
-        // Update the view's responseArea with the server's response
-    }
-
-    public void setChannel() {
-        // Retrieve the channel number from the view's channelField
-        // Communicate with model to set the channel to that number
-        // Update the view's responseArea with the server's response
+        model.channelDown();
     }
 }
