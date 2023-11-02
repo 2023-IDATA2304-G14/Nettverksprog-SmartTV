394568a009cbddd85f67e61d9ec7b0cf4fc424b3
Anders Lund <61545005+AnderzL7@users.noreply.github.com>
2023-11-02 15:42:37 +0100

    Adds variables and methods to connect RemoteModel to RemoteClient

--- src/main/java/no/ntnu/remote/gui/RemoteModel.java
+++ src/main/java/no/ntnu/remote/gui/RemoteModel.java
@@ -4,6 +4,11 @@ import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import no.ntnu.message.Command;
+import no.ntnu.message.SetChannelCommand;
+import no.ntnu.message.TurnOffCommand;
+import no.ntnu.message.TurnOnCommand;
+import no.ntnu.remote.RemoteClient;
 import no.ntnu.remote.RemoteClientListener;
 
 public class RemoteModel implements RemoteClientListener {
@@ -11,9 +16,16 @@ public class RemoteModel implements RemoteClientListener {
   private final BooleanProperty isOn = new SimpleBooleanProperty();
   private final IntegerProperty channelCount = new SimpleIntegerProperty();
   private final IntegerProperty currentChannel = new SimpleIntegerProperty();
+  private final RemoteClient client = new RemoteClient();
 
     public RemoteModel(RemoteView view) {
         this.view = view;
+        init();
+    }
+
+    private void init() {
+      client.startClient();
+      client.startListeningThread(this);
     }
 
     @Override
@@ -34,5 +46,37 @@ public class RemoteModel implements RemoteClientListener {
     @Override
     public void handleErrorMessage(String message) {
         view.showError(message);
+    }
+
+    public void turnOnTV() {
+      Command turnOnCommand = new TurnOnCommand();
+      client.sendCommand(turnOnCommand);
+//      TODO: Add errorhandling
+    }
+
+    public void turnOffTV() {
+      Command turnOffCommand = new TurnOffCommand();
+      client.sendCommand(turnOffCommand);
+//      TODO: Add errorhandling
+    }
+
+    public void channelUp() {
+      int channel = currentChannel.get() + 1;
+      if (channel > channelCount.get()) {
+        channel = 1;
+      }
+      Command setChannelCommand = new SetChannelCommand(channel);
+      client.sendCommand(setChannelCommand);
+//      TODO: Add errorhandling
+    }
+
+    public void channelDown() {
+      int channel = currentChannel.get() - 1;
+      if (channel < 1) {
+        channel = channelCount.get();
+      }
+      Command setChannelCommand = new SetChannelCommand(channel);
+      client.sendCommand(setChannelCommand);
+//      TODO: Add errorhandling
     }
 }
